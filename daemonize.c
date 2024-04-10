0a5b79858ad3f1311d92bae1df1a17d5c510befd
Tyler Connel <tyler@schedmd.com>
2024-04-10 21:08:40 +0000

    Hard tabs.

--- daemonize.c
+++ daemonize.c
@@ -17,72 +17,72 @@
 
 static void skeleton_daemon()
 {
-    pid_t pid;
+	pid_t pid;
 
-    /* Fork off the parent process */
-    pid = fork();
+	/* Fork off the parent process */
+	pid = fork();
 
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
+	/* An error occurred */
+	if (pid < 0)
+		exit(EXIT_FAILURE);
 
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
+	/* Success: Let the parent terminate */
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
 
-    /* On success: The child process becomes session leader */
-    if (setsid() < 0)
-        exit(EXIT_FAILURE);
+	/* On success: The child process becomes session leader */
+	if (setsid() < 0)
+		exit(EXIT_FAILURE);
 
-    /* Catch, ignore and handle signals */
-    //TODO: Implement a working signal handler */
-    signal(SIGCHLD, SIG_IGN);
-    signal(SIGHUP, SIG_IGN);
+	/* Catch, ignore and handle signals */
+	//TODO: Implement a working signal handler */
+	signal(SIGCHLD, SIG_IGN);
+	signal(SIGHUP, SIG_IGN);
 
-    /* Fork off for the second time*/
-    pid = fork();
+	/* Fork off for the second time*/
+	pid = fork();
 
-    /* An error occurred */
-    if (pid < 0)
-        exit(EXIT_FAILURE);
+	/* An error occurred */
+	if (pid < 0)
+		exit(EXIT_FAILURE);
 
-    /* Success: Let the parent terminate */
-    if (pid > 0)
-        exit(EXIT_SUCCESS);
+	/* Success: Let the parent terminate */
+	if (pid > 0)
+		exit(EXIT_SUCCESS);
 
-    /* Set new file permissions */
-    umask(0);
+	/* Set new file permissions */
+	umask(0);
 
-    /* Change the working directory to the root directory */
-    /* or another appropriated directory */
-    chdir("/");
+	/* Change the working directory to the root directory */
+	/* or another appropriated directory */
+	chdir("/");
 
-    /* Close all open file descriptors */
-    int x;
-    for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
-    {
-        close (x);
-    }
+	/* Close all open file descriptors */
+	int x;
+	for (x = sysconf(_SC_OPEN_MAX); x>=0; x--)
+	{
+		close (x);
+	}
 
-    /* Open the log file */
-    openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
+	/* Open the log file */
+	openlog ("firstdaemon", LOG_PID, LOG_DAEMON);
 }
 
 int main()
 {
-    skeleton_daemon();
+	skeleton_daemon();
 
-    while (1)
-    {
-        //TODO: Insert daemon code here.
-        syslog (LOG_NOTICE, "First daemon started.");
 	while (1)
-        	sleep (1);
-        break;
-    }
+	{
+		//TODO: Insert daemon code here.
+		syslog (LOG_NOTICE, "First daemon started.");
+	while (1)
+			sleep (1);
+		break;
+	}
 
-    syslog (LOG_NOTICE, "First daemon terminated.");
-    closelog();
+	syslog (LOG_NOTICE, "First daemon terminated.");
+	closelog();
 
-    return EXIT_SUCCESS;
+	return EXIT_SUCCESS;
 }
